54030988c67a65a983bc8d069abdc9cc395d0c2f
Morella92 <morena.piem@outlook.com>
2023-03-31 15:41:52 +0200

    FEAT products array

--- food.php
+++ food.php
@@ -4,7 +4,7 @@
         public $food_format;
         public $age;
     
-        public function __construct($_name, $_price, $_category, $weight, $food_format, $age) {
+        public function __construct($_name, $_price, $_category, $_weight, $_food_format, $_age) {
             parent::__construct($_name, $_price, $_category);
             $this->weight = $_weight;
             $this->food_format = $_food_format;
--- index.php
+++ index.php
@@ -1,7 +1,37 @@
 <?php
     include './product.php';
     include './food.php';
-    include './toy.php'
+    include './toy.php';
+    include './bed.php';
+    include './category.php';
+
+    // Istanze della classe Product
+    $product1 = new Product("Dog Leash", 5, "Accessories");
+    $product2 = new Product("Dog Coat", 20, "Abbigliamento");
+
+    // Istanze della classe Food
+    $food1 = new Food("Croquettes", 15, "Pet's Food", 2, "Dry", "Adult");
+    $food2 = new Food("Cat's Food", 10, "Pet's Food", 1, "Wet", "Puppy");
+
+    // Istanze della classe Toy
+    $toy1 = new Toy("Dog's Ball", 7, "Toys", "Plastica", "Adult");
+    $toy2 = new Toy("Mouse", 16, "Toys", "Peluche", "Puppy");
+
+    // Istanze della classe Bed
+    $bed1 = new Bed("Dog Pillow", 30, "Pet's Bed", "50x70");
+    $bed2 = new Bed("Cat House", 40, "Pet's Bed", "70x90");
+
+    $products = [
+        $product1, 
+        $product2, 
+        $food1, 
+        $food2, 
+        $toy1, 
+        $toy2, 
+        $bed1, 
+        $bed2
+    ];
+
 ?>
 
 <!DOCTYPE html>
